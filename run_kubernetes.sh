b8d72ed528656c6344a934d92cd9945a88a8750a
EC2 Default User <ec2-user@ip-172-31-29-146.ap-south-1.compute.internal>
2020-11-16 17:17:35 +0000

    updated kubernets

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=robinhalfbloodprince/mlpredict
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+#minikube kubectl run mlpredict --image=$dockerpath --port=80
+kubectl run mlpredict --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+#minikube kubectl port-forward mlpredict 8080:80
+kubectl port-forward mlpredict 8080:80
 
+# Log pod name
+minikube kubectl logs `minikube kubectl get pods -o=name`
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=robinhalfbloodprince/mlpredict
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username robinhalfbloodprince
+docker tag mlpredict $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
